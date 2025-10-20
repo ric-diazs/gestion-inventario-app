07353b3a00715d00e3f2619cf42f1cf5385aa796
Ricardo Díaz <ric.diazs@duocuc.cl>
2025-10-20 13:29:16 -0300

    android: agrega dependencia de vistas y navegacion

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -41,6 +41,11 @@ android {
 
 dependencies {
 
+    implementation("androidx.navigation:navigation-compose:2.9.5")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
